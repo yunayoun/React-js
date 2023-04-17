1343f6a028a917ea23a67ae7c2e544f07063cc93
yunayoun <nsook91@naver.com>
2023-04-18 00:24:35 +0900

    word-relay

---
+++ word-relay/client.jsx
@@ -0,0 +1,14 @@
+const React = require('react');
+const ReactDOM = require('react-dom/client');
+// const WordRelay = require('./WordRelay');
+const WordHook = require('./WordHook');
+
+// ReactDOM.createRoot(
+//   document.getElementById('root')
+// ).render(<WordRelay/>);
+
+
+
+ReactDOM.createRoot(
+  document.getElementById('root')
+).render(<WordHook/>);
