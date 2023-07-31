eae921070acc718fffd6afd04002550075539735
yunayoun <nsook91@naver.com>
2023-07-31 20:37:31 +0900

    material-ui

--- bord/src/components/Customer.js
+++ bord/src/components/Customer.js
@@ -1,32 +1,41 @@
 import react from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
 
 function Customer(props){
   return(
     <>
-      <CustomerProfile id={props.id} name={props.name} image={props.image}/>
-      <CustomerInfo gender={props.gender} birthday={props.birthday} job={props.job}/>
+      <TableRow>
+        <TableCell>{props.id}</TableCell>
+        <TableCell><img src={props.image} alt='profile'></img></TableCell>
+        <TableCell>{props.name}</TableCell>
+        <TableCell>{props.birthday}</TableCell>
+        <TableCell>{props.gender}</TableCell>
+        <TableCell>{props.job}</TableCell>
+      </TableRow>
     </>
 
   )
 };
 
-  function CustomerProfile(props){
-    return(
-      <>
-        <img src={props.image} alt='profile'/>
-        <h2>{props.name}({props.id})</h2>
-      </>
-    )
-  }
+  // function CustomerProfile(props){
+  //   return(
+  //     <>
+  //       <img src={props.image} alt='profile'/>
+  //       <h2>{props.name}({props.id})</h2>
+  //     </>
+  //   )
+  // }
 
-  function CustomerInfo(props){
-    return(
-      <>
-        <p>{props.birthday}</p>
-        <p>{props.gender}</p>
-        <p>{props.job}</p>
-      </>
-    )
-  }
+  // function CustomerInfo(props){
+  //   return(
+  //     <>
+  //       <p>{props.birthday}</p>
+  //       <p>{props.gender}</p>
+  //       <p>{props.job}</p>
+  //     </>
+  //   )
+  // }
 
 export default Customer;
